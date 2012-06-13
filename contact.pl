7ce66690a5c020247609ed2d971af8d9514c6d52
Héctor Valverde <hvalverde@uma.es>
2012-06-13 18:11:16 +0200

    Minor changes

--- contact.pl
+++ contact.pl
@@ -4,27 +4,42 @@
 use strict;
 use warnings;
 use Bio::Structure::Model;
-use Bio::Structure::Chain;
-use Bio::Structure::Entry;
 use Bio::Structure::IO::pdb;
 
 # Description ##################################################################
 # This script reads a PDB file and calculates the distance between each atom
 # from distinct chains.
-# It return (standar output) a table with the following structure (header):
+# It return two files:
+# FILE 1: detailed_file.txt ->
+# a table with the following structure (header):
 # Chain1|Chain2|AtomNumber(ResidueNumber)1|AtomNumber(ResidueNumber)2|Distance(A)
+# FILE 2: contact_file.txt ->
+# A table with the contact between residues insead atoms
 ###############################################################################
 
 # Pool of variables ###########################################################
 
  my $num_args = $#ARGV + 1;
  if ($num_args != 1) {
-  print "ERROR: Missing arguments.\nUsage: contact.pl pdbfile\n";
+  print "ERROR: Missing arguments.\nUsage: contact.pl <pdbfile>\n";
   exit;
  }
  
  # Input pdb file (just lines witch begin with "ATOM")
  my $input_file = $ARGV[0];
+ 
+ my $file_str = $input_file;
+    $file_str =~ s/\..*$//g;
+    $file_str =~ s/^.*\///g;
+ 
+ # File for atom contacts
+ my $output_detailed_file = ">detailed_".$file_str.".txt";
+ print $output_detailed_file;
+ open DETAILED, $output_detailed_file or die $!;
+ 
+ # File for residue contacts
+ my $output_contact_file = ">contact_".$file_str.".txt";
+ open CONTACT, $output_contact_file or die $!;
  
  # Distance Threshold
  my $dis_threshold = 4;
@@ -52,7 +67,7 @@ while (my $struc = $stream->next_structure) {
         
         # Chain identifier
         my $chainid = $chain->id;
-        print $chainid."\n";
+        print DETAILED "Chain ".$chainid."\n";
         
         # For remaining chains
         for (my $j=$i+1;$j<scalar @chains;$j++){
@@ -83,14 +98,16 @@ while (my $struc = $stream->next_structure) {
                             # Print always
                             if($all_distances){
                                 
-                                print $chain->id."\t".$chain2->id."\t".$atom->serial()." (".$atom->id."-".$res->id.")\t".$atom2->serial()." (".$atom->id."-".$res2->id.")\t".$distance."\n";
+                                print DETAILED $chain->id."\t".$chain2->id."\t".$atom->serial()." (".$atom->id."-".$res->id.")\t".$atom2->serial()." (".$atom->id."-".$res2->id.")\t".$distance."\n";
+                                print CONTACT $res->id."\t".$chain->id."\t".$res2->id."\t".$chain2->id."\n";
                                 
                             }else{
                                 # Print if < $dis_threshold
                                 if($distance < $dis_threshold){
                                 
-                                    print $chain->id."\t".$chain2->id."\t".$atom->serial()." (".$atom->id."-".$res->id.")\t".$atom2->serial()." (".$atom->id."-".$res2->id.")\t".$distance."\n";
-                                
+                                    print DETAILED $chain->id."\t".$chain2->id."\t".$atom->serial()." (".$atom->id."-".$res->id.")\t".$atom2->serial()." (".$atom->id."-".$res2->id.")\t".$distance."\n";
+                                    print CONTACT $res->id."\t".$chain->id."\t".$res2->id."\t".$chain2->id."\n";
+                                    
                                 }
                             }
                     
@@ -107,3 +124,5 @@ while (my $struc = $stream->next_structure) {
     }
     
 }
+
+close DETAILED; close CONTACT;
