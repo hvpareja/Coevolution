c9bbd0d74ddd8b68378be63f4cf57980879881f7
Héctor Valverde <hvalverde@uma.es>
2012-06-22 17:00:12 +0200

    Minor changes

--- clustal.sh
+++ clustal.sh
@@ -20,7 +20,7 @@ file=$(echo $input_fasta_file | sed "s/\.txt//g" )
 output_fasta_file=$(echo ${file} | sed "s/^.*\///g")
 
 
-clustalw -infile=$input_fasta_file -outfile=${output_fasta_file}_aligned.fa -output=pir
+clustalw -infile=$input_fasta_file -outfile=${output_fasta_file}_aligned.fa -output=pir -outorder=input
 
 clear
 echo -e "\nCLUSTAL REPORT:\n"
